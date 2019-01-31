31a1ec62d12e5393fb3ddadc2066a1e55ddc4ad7
SimonO <simonozzy@gmail.com>
2019-01-31 18:52:33 +0000

    Strong soap testing

--- src/server.js
+++ src/server.js
@@ -6,34 +6,34 @@ const bodyParser = require('body-parser');
 // const { db } = require('./dbConnection.js');
 const config = require('../config');
 const request = require('request');
-// const strongSoap = require('strong-soap').soap;
 
 // wsdl of the web service this client is going to invoke. For local wsdl you can use, url = './wsdls/stockquote.wsdl'
 // ## From here ON tests alternative strong-soap package vs older "plain" soap
-// "use strict";
-// const strongSoap = require('strong-soap').soap;
-// const url = 'http://www.webservicex.net/stockquote.asmx?WSDL';
+"use strict";
+const strongSoap = require('strong-soap').soap;
+const url = 'http://www.webservicex.net/stockquote.asmx?WSDL';
 
-// const requestArgs = {
-//   symbol: 'IBM'
-// };
+const requestArgs = {
+  symbol: 'IBM'
+};
 
-// const options = {};
-// strongSoap.createClient(url, options, function(err, client) {
-//   let method = client['StockQuote']['StockQuoteSoap']['GetQuote'];
-//   method(requestArgs, function(err, result, envelope, soapHeader) {
-//     //response envelope
-//     console.log('Response Envelope: \n' + envelope);
-//     //'result' is the response body
-//     console.log('Result: \n' + JSON.stringify(result));
-//   });
-// });  // ## From here UP tests  alternative strong-soap package vs older "plain" soap
+const options = {};
+strongSoap.createClient(url, options, function(err, client) {
+  let method = client['StockQuote']['StockQuoteSoap']['GetQuote'];
+  method(requestArgs, function(err, result, envelope, soapHeader) {
+    //response envelope
+    console.log('strongSoap Response Envelope: \n' + envelope);
+    //'result' is the response body
+    console.log('Result: \n' + JSON.stringify(result));
+  });
+});  // ## From here UP tests  alternative strong-soap package vs older "plain" soap
+
 
 const soapHeader = `<AccessToken><TokenValue>${config.source.accessToken}</TokenValue></AccessToken>`;
 
 app.use(bodyParser.json());
 
-// GetArrivalBoard
+// GetArrivalBoard - requires req to be passed in via the api call as JSON in a defined format
 router.post('/getArrivalBoard', (req, res) => {
   soap.createClient(config.source.soapUrl, (err, client) => {
     // console.log(err);
@@ -44,6 +44,10 @@ router.post('/getArrivalBoard', (req, res) => {
         console.log(err);
         res.sendStatus(400);
       } else {
+    //response envelope
+    console.log('Soap getArrivalBoard Response Envelope: \n' + envelope);
+    //'result' is the response body
+    console.log('Result: \n' + JSON.stringify(result));
         res.json(result);
       }
     });
@@ -57,7 +61,7 @@ const payload = {
   "timeWindowInMins": 30
 } ;
 
-// GetStationArrivals
+// GetStationArrivals - use "payload" as saved constant to pass as req 
 router.post('/GetStationArrivals/', (req, res) => {
   soap.createClient(config.source.soapUrl, (err, client) => {
     client.addSoapHeader(soapHeader);
@@ -66,6 +70,9 @@ router.post('/GetStationArrivals/', (req, res) => {
         console.log(err);
         res.sendStatus(400);
       } else {
+    console.log('Soap GetStationArrivals Response Envelope: \n' + envelope);
+    //'result' is the response body
+    console.log('Result: \n' + JSON.stringify(result));
         res.json(result);
         console.log( res.json(result) ) ;
       }
@@ -88,14 +95,15 @@ app.listen(config.server.port, () => {
 // request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true },  (err, res, body) => {
   request('http://localhost:8080/api/GetStationArrivals', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
-    console.dir(body);
-    // console.log(body.url);
+    // console.dir(body);
+    console.log(body.url);
 // console.log(body.explanation);
 });
 
 request(
 { method: 'POST'
-, url: 'http://localhost:8080/api/getArrivalBoard' 
+// , url: 'http://localhost:8080/api/getArrivalBoard' 
+, url: 'getArrivalBoard' 
 , multipart:
   [ { 'content-type': 'application/json'
     ,  body: JSON.stringify(
@@ -117,5 +125,5 @@ request(
     console.log(body)
   }
 }
-)
+);
 
